0217851a5588948348ce841d87bef2738e883fde
Akshit <akshit07032001@gmail.com>
2025-12-14 20:27:49 +0530

    commit: usage controller

--- src/main/java/com/lovable_clone/lovable_clone/service/UsageService.java
+++ src/main/java/com/lovable_clone/lovable_clone/service/UsageService.java
@@ -2,11 +2,10 @@ package com.lovable_clone.lovable_clone.service;
 
 import com.lovable_clone.lovable_clone.dto.subscription.PlanLimitsResponse;
 import com.lovable_clone.lovable_clone.dto.subscription.UsageTodayResponse;
-import org.jspecify.annotations.Nullable;
 
 public interface UsageService {
 
     UsageTodayResponse getTodayUsageOfUser(Long userId);
 
-    @Nullable PlanLimitsResponse getCurrentSubscriptionLimitsOfUser(Long userId);
+    PlanLimitsResponse getCurrentSubscriptionLimitsOfUser(Long userId);
 }
