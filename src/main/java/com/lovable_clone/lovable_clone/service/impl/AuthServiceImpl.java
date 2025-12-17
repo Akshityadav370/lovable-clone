db8cf9097a90e45873042209b1280f69e3c5276e
Akshit <akshit07032001@gmail.com>
2025-12-17 14:20:25 +0530

    add: services & db connection

---
+++ src/main/java/com/lovable_clone/lovable_clone/service/impl/AuthServiceImpl.java
@@ -0,0 +1,20 @@
+package com.lovable_clone.lovable_clone.service.impl;
+
+import com.lovable_clone.lovable_clone.dto.auth.AuthResponse;
+import com.lovable_clone.lovable_clone.dto.auth.LoginRequest;
+import com.lovable_clone.lovable_clone.dto.auth.SignupRequest;
+import com.lovable_clone.lovable_clone.service.AuthService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    @Override
+    public AuthResponse signup(SignupRequest request) {
+        return null;
+    }
+
+    @Override
+    public AuthResponse login(LoginRequest request) {
+        return null;
+    }
+}
---
+++ src/main/java/com/lovable_clone/lovable_clone/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.lovable_clone.lovable_clone.service.impl;
+
+import com.lovable_clone.lovable_clone.dto.auth.UserProfileResponse;
+import com.lovable_clone.lovable_clone.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserProfileResponse getProfile(Long userId) {
+        return null;
+    }
+}
